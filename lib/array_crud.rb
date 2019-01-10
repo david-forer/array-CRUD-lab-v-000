f771ab910db6439db93860d388c12278ad121417
David Forer <david.j.forer@gmail.com>
2019-01-10 10:13:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [dave, kalpana, derrick, roger]
+  ["dave", "kalpana", "derrick", "roger"]
 end
 
 def add_element_to_end_of_array(array, element)
